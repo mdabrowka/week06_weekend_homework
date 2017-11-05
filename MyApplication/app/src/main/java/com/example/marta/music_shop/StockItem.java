f7c8385fdd8a047c134e101d20c3f3f8d3387fac
Marta <martadabrowka@gmail.com>
2017-11-05 16:18:58 +0000

    created abstract class StockItem:

---
+++ MyApplication/app/src/main/java/com/example/marta/music_shop/StockItem.java
@@ -0,0 +1,12 @@
+package com.example.marta.music_shop;
+
+/**
+ * Created by marta on 05/11/2017.
+ */
+
+public abstract class StockItem implements Sellable {
+    private String description;
+    private int buyPrice;
+    private int sellPrice;
+
+}
