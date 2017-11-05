90d8950bbbf0c5dc6a0a8b409ca1cc39003aa3f9
Marta <martadabrowka@gmail.com>
2017-11-05 21:28:41 +0000

    added a misc, prineted and music sheet class

---
+++ MyApplication/app/src/main/java/com/example/marta/music_shop/MusicSheet.java
@@ -0,0 +1,12 @@
+package com.example.marta.music_shop;
+
+/**
+ * Created by marta on 05/11/2017.
+ */
+
+public class MusicSheet extends Printed {
+    public String title;
+    public String composer;
+
+
+}
