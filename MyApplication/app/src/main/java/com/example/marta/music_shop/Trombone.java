d8cf587647a5dbf3198bba72f3a1a7760ec47ac9
Marta <martadabrowka@gmail.com>
2017-11-05 16:06:49 +0000

    tested and passed trombone

--- MyApplication/app/src/main/java/com/example/marta/music_shop/Trombone.java
+++ MyApplication/app/src/main/java/com/example/marta/music_shop/Trombone.java
@@ -7,6 +7,16 @@ package com.example.marta.music_shop;
  */
 
 public class Trombone extends Brass {
+    private String type;
+
+    public Trombone(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
     @Override
     public String play() {
         return "Transforming the air into sound";
---
+++ MyApplication/app/src/test/java/com/example/marta/music_shop/TromboneTest.java
@@ -0,0 +1,35 @@
+package com.example.marta.music_shop;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by marta on 05/11/2017.
+ */
+
+public class TromboneTest {
+    Trombone trombone;
+
+    @Before
+    public void before() {
+        trombone = new Trombone("bass");
+    }
+
+
+    @Test
+    public void play() {
+        assertEquals("Transforming the air into sound", trombone.play());
+    }
+
+    @Test
+    public void getType() {
+        assertEquals("bass", trombone.getType());
+
+
+    }
+
+}
