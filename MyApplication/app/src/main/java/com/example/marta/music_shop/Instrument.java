4118db9ed2af3db5055948edb4bece61bf91eaff
Marta <martadabrowka@gmail.com>
2017-11-05 21:01:49 +0000

    refactored string, keyboard and brass classes to extend instrument

--- MyApplication/app/src/main/java/com/example/marta/music_shop/Instrument.java
+++ MyApplication/app/src/main/java/com/example/marta/music_shop/Instrument.java
@@ -4,5 +4,5 @@ package com.example.marta.music_shop;
  * Created by marta on 05/11/2017.
  */
 
-public abstract class Instrument extends StockItem {
+public abstract class Instrument extends StockItem implements Playable{
 }
